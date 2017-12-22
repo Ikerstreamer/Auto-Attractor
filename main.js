abf9cd7c9cdc683c421320d7ebd372ab79bc64ba
Ikerstreamer <ikerdeaguirre@hotmail.com>
2017-12-22 12:36:47 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var autogen = true;
 var autoprest = 2;
+var d1 = document.getElementById('generatorsTab');
+d1.insertAdjacentHTML('beforeend', '<form action="/action_page.php"> Auto-Generator: <input type="checkbox" id="genautoselect"><br>Auto-Prestige: <input type="text" id="prestautoamnt" defaultValue="0"></form>');
 function StartAutoAttractor(){
 var autoprestige = setInterval(function(){
 if(getPrestigePower().gte(player.prestigePower.times(autoprest))) reset(1);
 },50)
 var autogenerator = setInterval(maxAll,50)
-var d1 = document.getElementById('generatorsTab');
-d1.insertAdjacentHTML('beforeend', '<form action="/action_page.php"> Auto-Generator: <input type="checkbox" id="genautoselect"><br>Auto-Prestige: <input type="text" id="prestautoamnt"></form>');
 }
 setInterval(function(){
   autogen = document.getElementById("genautoselect").checked;
