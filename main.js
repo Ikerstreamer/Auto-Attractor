f93b78a1e3c661e4e2ae9fa2ef0f2fc81a7374a6
Ikerstreamer <ikerdeaguirre@hotmail.com>
2017-12-22 11:17:59 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var autogen = true;
 var autoprest = 2;
 function StartAutoAttractor(){
 var autoprestige = setInterval(function(){
-if(getPrestigePower().gte(player.prestigePower.times(autoprest))) reset(1);
+if(getPrestigePower().gte(player.prestigePower.times(autoprest))) reset(1)
 },50)
 var autogenerator = setInterval(maxAll,50)
 }
@@ -19,7 +19,7 @@ if(autoprest<=0)
 clearInterval(autoprestige)
 }else(
 autoprestige = setInterval(function(){
-if(getPrestigePower().gte(player.prestigePower.times(autoprest))) reset(1);
+if(getPrestigePower().gte(player.prestigePower.times(autoprest))) reset(1)
 },50)
 )
 },50)
