42badeb4929b68c7f9d08083f53883a72da03630
Ikerstreamer <ikerdeaguirre@hotmail.com>
2017-12-24 10:33:21 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,22 +3,21 @@ var autoprest = 2;
 var Tab= document.getElementById('generatorsTab');
 Tab.insertAdjacentHTML('beforeend', '<form action="/action_page.php"> Auto-Generator: <input type="checkbox" id="genautoselect"><br>Auto-Prestige: <input type="text" id="prestautoamnt" defaultValue="0"></form><Button onclick="StartAutoAttractor()">Start</Button><br><span>Auto Attarctor V0.2<br>by IkerStream</span>')
 
+function Update(){
+        setInterval(Loop,50);
+        autogen = document.getElementById("genautoselect").checked;
+    if(parseDouble(document.getElementById("prestautoamnt").value)!="NaN")autoprest = parseDouble(document.getElementById("prestautoamnt").value);
+}
+
 function Autoprestige(){
         if (getPrestigePower().gte(player.prestigePower.times(autoprest))) reset(1);
-    
-    setInterval(Loop, 50)
 }
 
 function AutoGenerator(){
-for(i=10;i>0;i++)
-{
-buyGenerator(i,100);
-}
+for(i=10;i>0;i++) buyGenerator(i,100);
 }
 
 function Loop() {
-    autogen = document.getElementById("genautoselect").checked;
-    if(parseDouble(document.getElementById("prestautoamnt").value)!="NaN")autoprest = parseDouble(document.getElementById("prestautoamnt").value);
     if (autogen) AutoGenerator();
     if (autoprest > 1) AutoPrestige();
 }
