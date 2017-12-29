a2685ff0d96a0038f57a4e5f1b94cbabbffcda6e
Ikerstreamer <ikerdeaguirre@hotmail.com>
2017-12-29 09:33:10 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,49 @@
 var autogen = true;
 var autoprest = 0;
+var prestTime = 0;
 var autotrans = 0;
+var prestPerSec = 0;
+var dprest = false;
 var Tab = document.getElementById('generatorsTab');
-Tab.insertAdjacentHTML('beforeend', '<form action="/action_page.php"> Auto-Generator: <input type="checkbox" id="genautoselect"><br>Auto-Prestige: <input type="text" id="prestautoamnt" defaultValue="0"><br>Auto-Transfer: <input type="text" id="transautoamnt" defaultValue="0"></form><Button onclick="UpdateAA()">Start</Button><br><span>Auto Attractor V0.5<br>by IkerStream</span>')
+Tab.insertAdjacentHTML('beforeend', '<form action="/action_page.php"> Auto-Generator: <input type="checkbox" id="genautoselect"><br>Auto-Prestige: <input type="text" id="prestautoamnt" defaultValue="0">Dynamic-Prestige: <input type="checkbox" id="dynamicprestselect"><br>Auto-Transfer: <input type="text" id="transautoamnt" defaultValue="0"></form><Button onclick="UpdateAA()">Start</Button><br><span>Auto Attractor V0.5<br>by IkerStream</span>')
 
 function UpdateAA() {
     setInterval(Loop, 50);
     autogen = document.getElementById("genautoselect").checked;
+    dprest = document.getElementById("dynamicprestselect").checked;
     if (!isNaN(document.getElementById("prestautoamnt").value)) autoprest = parseFloat(document.getElementById("prestautoamnt").value);
     if (!isNaN(document.getElementById("transautoamnt").value)) autotrans = parseInt(document.getElementById("transautoamnt").value);
 }
 
 function AutoPrestige() {
-    if (getPrestigePower().gte(player.prestigePower.times(autoprest))) reset(1);
+    if (getPrestigePower().gte(player.prestigePower.times(autoprest))){ 
+        reset(1);
+        prestTime = 0;
+    }
     else {
         num = Math.pow(autotrans, 3)
-        if (player.prestigeUpgrades.includes(13) && getPrestigePower().gt(num * 500)) reset(1);
-        else if (getPrestigePower().gt(num * 1000)) reset(1);
+        if (player.prestigeUpgrades.includes(13) && getPrestigePower().gt(num * 500)){
+            reset(1);
+            prestTime = 0;
+        }
+        else if (getPrestigePower().gt(num * 1000)) {
+            reset(1);
+            prestTime = 0;   
+        }
     }
+}
+
+function DynamicAutoPrestige()
+{
+if(getPrestigePower().gt(player.prestigePower))
+{
+    x = getPrestigePower().divide(prestTime);
+    if(x<prestPerSec)
+    {
+        reset(1);
+        prestTime = 0;
+    }
+}
 }
 
 function AutoGenerator() {
@@ -30,6 +56,11 @@ function AutoTransfer() {
 
 function Loop() {
     if (autogen) AutoGenerator();
-    if (autoprest > 1) AutoPrestige();
+    if (dprest = true)DynamicAutoPrestige();
+    else if (autoprest > 1) AutoPrestige();
     if (autotrans >= 1) AutoTransfer();
+    document.getElementById("pt1").onclick = function() {
+    prestTime = 0;
+    }
+    prestTime+=0.05;
 }
