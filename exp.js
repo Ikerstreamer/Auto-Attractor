8f4f3e99100c2522f30b598dc5613fc22994cce7
Ikerstreamer <ikerdeaguirre@hotmail.com>
2018-01-02 21:59:59 -0600

    Update exp.js

--- exp.js
+++ exp.js
@@ -124,7 +124,16 @@ function DynamicAutoPrestige() {
 }
 
 function AutoGenerator() {
-    for (i = 10; i > 0; i--) buyGen(i,10);
+    buyGen(10,10);
+    buyGen(9,10);
+    buyGen(8,10);
+    buyGen(7,10);
+    buyGen(6,10);
+    buyGen(5,10);
+    buyGen(4,10);
+    buyGen(3,10);
+    buyGen(2,10);
+    buyGen(1,10);
 }
 
 function AutoTransfer() {
